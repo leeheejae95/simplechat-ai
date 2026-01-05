5891772a4a67c985b33ac983156bf77149e1c4a4
leehj <leeheejae55@naver.com>
2026-01-05 18:33:52 +0900

    feat: AI 응답 실시간 스트리밍(SSE) 간단 API 구현

--- src/main/java/org/ai/simpleaichat/rest/SimpleChatController.java
+++ src/main/java/org/ai/simpleaichat/rest/SimpleChatController.java
@@ -1,8 +1,11 @@
 package org.ai.simpleaichat.rest;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class SimpleChatController {
@@ -16,5 +19,15 @@ public class SimpleChatController {
     @GetMapping("ai")
     String generation(String userPrompts) {
         return this.chatClient.prompt().user(userPrompts).call().content();
+    }
+
+    @GetMapping(value = "call", produces = MediaType.APPLICATION_JSON_VALUE)
+    ChatResponse call(String userPrompts) {
+        return this.chatClient.prompt().user(userPrompts).call().chatResponse();
+    }
+
+    @GetMapping(value = "stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    Flux<String> stream(String userPrompts) {
+        return this.chatClient.prompt().user(userPrompts).stream().content();
     }
 }
